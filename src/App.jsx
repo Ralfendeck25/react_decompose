eed8ec260c9e4fca0f3675a252ac6316338cfcc7
Peterson <poseidoncenghizerzhemetmhossad@outlook.com>
2025-04-23 05:32:54 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import Header from './components/Header/Header';
+import Welcome from './components/Welcome/Welcome';
+import Article from './components/Article/Article';
 import './App.css';
+
 // Move each BEM block to a separate component (file) and import them here
 
 function App() {
